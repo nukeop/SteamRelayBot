1a66084c428abb4f1d43d09fbbe97dd101429b38
barackobama <alek.poczta@gmail.com>
2015-11-24 23:26:59 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # SteamRelayBot
+
+A bot for Steam that sits in a group chat and relays messages between the group chat and a chosen IRC channel. Also logs the chat to a file and responds to some simple commands.
+
+Uses this:
+https://github.com/SteamRE/SteamKit
---
+++ SteamRelayBot/Program.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+using SteamKit2;
+
+namespace SteamRelayBot
+{
+    class Logger
+    {
+
+        string filename;
+
+        public Logger(string f)
+        {
+            filename = f;
+        }
+
+        public void LogMessage(string message, string level)
+        {
+            System.IO.StreamWriter file = new System.IO.StreamWriter(filename, true);
+
+            message = DateTime.Now + " - " + level +" - " + message;
+
+            Console.WriteLine(message);
+
+            file.WriteLine(message);
+
+            file.Close();
+        }
+
+        public void Debug(string message)
+        {
+            LogMessage(message, "DEBUG");
+        }
+
+        public void Info(string message)
+        {
+            LogMessage(message, "INFO");
+        }
+
+        public void Warning(string message)
+        {
+            LogMessage(message, "WARNING");
+        }
+
+        public void Error(string message)
+        {
+            LogMessage(message, "ERROR");
+        }
+
+    }
+
+    class Program
+    {
+        static SteamClient steamClient;
+        static CallbackManager manager;
+
+        static SteamUser steamUser;
+        static SteamFriends steamFriends;
+
+        static bool isRunning;
+
+        static string user, pass, authCode = "";
+
+        static Logger log;
+
+        static List<SteamID> greeted;
+
+        static void Main(string[] args)
+        {
+            log = new Logger("relaybot.log");
+
+            greeted = new List<SteamID>();
+
+            if (args.Length < 2)
+            {
+                log.Error("No username and password specified!");
+                return;
+            }
+
+            user = args[0];
+            pass = args[1];
+
+            steamClient = new SteamClient(System.Net.Sockets.ProtocolType.Tcp);
+            manager = new CallbackManager(steamClient);
+
+            steamUser = steamClient.GetHandler<SteamUser>();
+            steamFriends = steamClient.GetHandler<SteamFriends>();
+
+            manager.Subscribe<SteamClient.ConnectedCallback>(OnConnected);
+            manager.Subscribe<SteamClient.DisconnectedCallback>(OnDisconnected);
+
+            manager.Subscribe<SteamUser.LoggedOnCallback>(OnLoggedOn);
+            manager.Subscribe<SteamUser.LoggedOffCallback>(OnLoggedOff);
+
+            manager.Subscribe<SteamUser.AccountInfoCallback>(OnAccountInfo);
+            manager.Subscribe<SteamFriends.FriendsListCallback>(OnFriendsList);
+            manager.Subscribe<SteamFriends.FriendAddedCallback>(OnFriendAdded);
+
+            manager.Subscribe<SteamFriends.ChatInviteCallback>(OnChatInvite);
+            manager.Subscribe<SteamFriends.ChatEnterCallback>(OnChatEnter);
+            manager.Subscribe<SteamFriends.FriendMsgCallback>(OnFriendMessage);
+
+            manager.Subscribe<SteamFriends.ChatMsgCallback>(OnChatroomMessage);
+
+            isRunning = true;
+
+            log.Info("Connecting to Steam...");
+
+            steamClient.Connect();
+
+            //callback loop
+            while (isRunning)
+            {
+                manager.RunWaitCallbacks(TimeSpan.FromSeconds(1));
+            }
+        }
+
+        static void OnConnected(SteamClient.ConnectedCallback callback)
+        {
+            if (callback.Result != EResult.OK)
+            {
+                log.Error(String.Format("Unable to connect to Steam: {0}", callback.Result));
+
+                isRunning = false;
+                return;
+            }
+
+            log.Info(String.Format("Connected to Steam! Logging in '{0}'...", user));
+
+            steamUser.LogOn(new SteamUser.LogOnDetails
+            {
+                Username = user,
+                Password = pass,
+            });
+        }
+
+        static void OnDisconnected(SteamClient.DisconnectedCallback callback)
+        {
+            log.Info("Disconnected from Steam");
+
+            isRunning = false;
+        }
+
+        static void OnLoggedOn(SteamUser.LoggedOnCallback callback)
+        {
+            if (callback.Result != EResult.OK)
+            {
+                if (callback.Result == EResult.AccountLogonDenied)
+                {
+
+                    log.Error("Unable to logon to Steam: This account is SteamGuard protected.");
+
+                    isRunning = false;
+
+                    return;
+                }
+
+                log.Error(String.Format("Unable to logon to Steam: {0} / {1}", callback.Result, callback.ExtendedResult));
+
+                isRunning = false;
+                return;
+            }
+
+            log.Info("Successfully logged on!");
+        }
+
+        static void OnAccountInfo(SteamUser.AccountInfoCallback callback)
+        {
+            steamFriends.SetPersonaState(EPersonaState.Online);
+        }
+
+        static void OnFriendsList(SteamFriends.FriendsListCallback callback)
+        {
+            int friendCount = steamFriends.GetFriendCount();
+
+            log.Info(String.Format("We have {0} friends", friendCount));
+
+            for (int x = 0; x < friendCount; x++)
+            {
+                SteamID steamIdFriend = steamFriends.GetFriendByIndex(x);
+
+                log.Info(String.Format("Friend: {0}", steamIdFriend.Render()));
+            }
+
+            foreach (var friend in callback.FriendList)
+            {
+                if (friend.Relationship == EFriendRelationship.RequestRecipient)
+                {
+                    // add everyone who invited us to friends
+                    log.Info(String.Format("User {0} added me to his/her friends", friend.SteamID.Render()));
+                    steamFriends.AddFriend(friend.SteamID);
+                }
+            }
+        }
+
+        static void OnFriendAdded(SteamFriends.FriendAddedCallback callback)
+        {
+            log.Info(String.Format("{0} is now a friend", callback.PersonaName));
+        }
+
+        static void OnLoggedOff(SteamUser.LoggedOffCallback callback)
+        {
+            log.Info(String.Format("Logged off of Steam: {0}", callback.Result));
+        }
+
+        static void OnChatInvite(SteamFriends.ChatInviteCallback callback)
+        {
+            log.Info(String.Format("Invited to {0} by {1}", callback.ChatRoomName, steamFriends.GetFriendPersonaName(callback.PatronID)));
+            steamFriends.JoinChat(callback.ChatRoomID);
+        }
+
+        static void OnFriendMessage(SteamFriends.FriendMsgCallback callback)
+        {
+            if (callback.EntryType.Equals(EChatEntryType.Typing))
+                log.Info(String.Format("{0} started typing a message to me", steamFriends.GetFriendPersonaName(callback.Sender)));
+
+            if (callback.EntryType.Equals(EChatEntryType.ChatMsg))
+                log.Info(String.Format("Message from {0}: {1}", steamFriends.GetFriendPersonaName(callback.Sender), callback.Message));
+        }
+
+        static void OnChatEnter(SteamFriends.ChatEnterCallback callback)
+        {
+            if (callback.EnterResponse == EChatRoomEnterResponse.NotAllowed)
+            {
+                log.Warning(String.Format("Not allowed to join {}", callback.ChatID));
+            }
+
+            steamFriends.SendChatRoomMessage(callback.ChatID, EChatEntryType.ChatMsg, String.Format("RelayBot™ signed in and joined chatroom: {0}", callback.ChatRoomName));
+            log.Info(String.Format("[[ME]]: RelayBot™ signed in and joined chatroom: {0}", callback.ChatRoomName));
+
+        }
+
+        static void OnChatroomMessage(SteamFriends.ChatMsgCallback callback)
+        {
+            if(callback.ChatMsgType.Equals(EChatEntryType.ChatMsg))
+                log.Info(String.Format("{0}[[{1}]]: {2}", steamFriends.GetFriendPersonaName(callback.ChatterID), callback.ChatterID.Render(), callback.Message));
+
+            if (callback.Message == "hi" || callback.Message == "hello" || callback.Message == "hey")
+            //if (callback.ChatMsgType.Equals(EChatEntryType.Entered))
+            {
+                log.Info(String.Format("{0} entered group chat: {1}", steamFriends.GetFriendPersonaName(callback.ChatterID), steamFriends.GetClanName(callback.ChatRoomID)));
+                if (!greeted.Contains(callback.ChatterID))
+                {
+                    steamFriends.SendChatRoomMessage(callback.ChatRoomID, EChatEntryType.ChatMsg, String.Format("Hello {0}", steamFriends.GetFriendPersonaName(callback.ChatterID)));
+                    log.Info(String.Format("[[ME]]: Hello {0}", steamFriends.GetFriendPersonaName(callback.ChatterID)));
+                    greeted.Add(callback.ChatterID);
+                }
+            }
+
+            if (callback.ChatMsgType.Equals(EChatEntryType.Disconnected) || callback.ChatMsgType.Equals(EChatEntryType.LeftConversation))
+                log.Info(String.Format("{0}[[{1}]] left the chat", steamFriends.GetFriendPersonaName(callback.ChatterID), callback.ChatterID.Render()));
+
+            if (callback.Message.Contains("what's the count") || callback.Message.Contains("whats the count"))
+            {
+                steamFriends.SendChatRoomMessage(callback.ChatRoomID, EChatEntryType.ChatMsg, "good. real good");
+                log.Info("[[ME]]: good. real good");
+            }
+        }
+
+        
+    }
+}
