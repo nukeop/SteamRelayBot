46732187a9840eb9c51a51c5c11ca969a0c74bef
nukeop <alek.poczta@gmail.com>
2016-04-23 01:18:57 +0100

    Add autojoining function

---
+++ SteamRelayBot/ConfigElements.cs
@@ -0,0 +1,51 @@
+﻿using System.Configuration;
+
+namespace SteamRelayBot
+{
+    class GroupChatElement : ConfigurationElement
+    {
+        [ConfigurationProperty("steamID", IsKey = true, IsRequired = true)]
+        public string SteamID
+        {
+            get { return (string)this["steamID"]; }
+        }
+    }
+
+    class GroupChatElementCollection : ConfigurationElementCollection
+    {
+        public GroupChatElement this[int index]
+        {
+            get
+            {
+                return (GroupChatElement)BaseGet(index);
+            }
+            set
+            {
+                if (BaseGet(index) != null)
+                {
+                    BaseRemoveAt(index);
+                }
+                BaseAdd(index, value);
+            }
+        }
+
+        protected override ConfigurationElement CreateNewElement()
+        {
+            return new GroupChatElement();
+        }
+
+        protected override object GetElementKey(ConfigurationElement element)
+        {
+            return ((GroupChatElement)element).SteamID;
+        }
+    }
+
+    class AutojoinGroupChatConfigurationSection : ConfigurationSection
+    {
+        [ConfigurationProperty("steamIDs")]
+        public GroupChatElementCollection SteamIDs
+        {
+            get { return (GroupChatElementCollection)this["steamIDs"]; }
+        }
+    }
+}
